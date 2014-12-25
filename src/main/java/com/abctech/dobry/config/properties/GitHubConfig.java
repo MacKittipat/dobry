aa46e155e42c65ab57ceec6cf458616204e62629
Mac <mac@abctech-thailand.com>
2014-12-25 15:32:47 +0700

    Add config to github.properties

--- src/main/java/com/abctech/dobry/config/properties/GitHubConfig.java
+++ src/main/java/com/abctech/dobry/config/properties/GitHubConfig.java
@@ -14,11 +14,39 @@ public class GitHubConfig {
     @Value("${github.url.access-token}")
     private String accessTokenUrl;
 
+    @Value("${github.app.client.id}")
+    private String appClientId;
+
+    @Value("${github.app.client.secret}")
+    private String appClientSecret;
+
+    @Value("${github.authorize.scope}")
+    private String authorizeScope;
+
+    @Value("${github.authorize.state}")
+    private String authorizeState;
+
     public String getAuthorizeUrl() {
         return authorizeUrl;
     }
 
     public String getAccessTokenUrl() {
         return accessTokenUrl;
+    }
+
+    public String getAppClientId() {
+        return appClientId;
+    }
+
+    public String getAppClientSecret() {
+        return appClientSecret;
+    }
+
+    public String getAuthorizeScope() {
+        return authorizeScope;
+    }
+
+    public String getAuthorizeState() {
+        return authorizeState;
     }
 }
