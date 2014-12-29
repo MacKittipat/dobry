9b2c7702854040e4af7a888e2578d933df45cb0e
aun <aun.sira@gmail.com>
2014-12-29 14:15:56 +0700

    Add GitHub login interceptor

---
+++ src/main/java/com/abctech/dobry/interceptor/GitHubInterceptor.java
@@ -0,0 +1,31 @@
+package com.abctech.dobry.interceptor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+public class GitHubInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        // exclude the root web context and the login event url
+        if (!request.getRequestURI().equals("/dobry/") &&
+            !request.getRequestURI().equals("/dobry/auth/github") &&
+            !request.getRequestURI().equals("/dobry/auth/github/callback")) {
+            String accessToken = null;
+            try {
+                accessToken = request.getSession().getAttribute("accessToken").toString();
+            }  catch (NullPointerException ex) {
+                response.sendRedirect("/dobry/");
+            }
+            if (accessToken != null) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+}
